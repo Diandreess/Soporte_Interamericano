133f96cc4ea0a429eeab59b82ca008e5f06b3772
DiegoBautista <dabautistac@correo.url.edu.gt>
2024-03-31 12:40:30 -0600

    modificación de estilos

--- Codigo Fuente/Aplicacion_Programa_Soporte_Interamericano/src/Principal/Add_Student/Add_Student.jsx
+++ Codigo Fuente/Aplicacion_Programa_Soporte_Interamericano/src/Principal/Add_Student/Add_Student.jsx
@@ -6,6 +6,7 @@ import student2 from '../../Assets/Images/vmbautistac.png'
 import student3 from '../../Assets/Images/cfgonzalesc.png'
 import student4 from '../../Assets/Images/jmmarroquinr.png'
 import student5 from '../../Assets/Images/pcpisquiyp.png'
+
 export class Add_Student extends Component {
 
     constructor(props) {
@@ -96,12 +97,11 @@ export class Add_Student extends Component {
                 loadStudentInformation: true,
                 disableCreateButton: false
             }))
-            alert(`Se creó alumno ${this.state.actualStudent.name}`)
         } else {
             this.setState({
-                loadStudentInformation: false
+                loadStudentInformation: false,
+                disableCreateButton: true
             })
-            alert("Este estudiante no existe")
         }
     }
 
@@ -115,49 +115,46 @@ export class Add_Student extends Component {
         const { studentemail, actualStudent, validateMessage: messageValidate, disableValidButton: validButtonState, disableCreateButton: createButtonState, loadStudentInformation: loadComponent } = this.state
 
         return (
-            <div className='AddStudentContainer container-md row-gap-3'>
-                <h1 className='display-5'>Add Student</h1>
+            <div className='AddStudentContainer container-md row-gap-3 mt-5'>
+                <h1 className='display-6'>Add Student</h1>
                 <div id='validate' className='container '>
-                <div className='d-flex justify-content-center align-items-end '>
-               <div className='form-group'>
-               <p className='lead'>
-               Estudent email 
-               <small className='text-danger'>*</small>
-               </p>
-                            <input id='studentEmailID' type='text' placeholder='Enter an email' value={studentemail} onChange={this.handleStudentEmail} />
-               </div>
-                           <div>
-                           <button className='btn btn-success mx-2' id='validateButtonID' onClick={this.handleValidateStudent} disabled={validButtonState}>Validate</button>
-                           </div>
-                </div>
+                    <div className='d-flex justify-content-center align-items-end'>
+                        <div className='form-group'>
+                            <p>
+                                Estudent email
+                                <small className='text-danger'>*</small>
+                            </p>
+                            <input id='studentEmailID' type='text' className='form-control' placeholder='Enter an email' value={studentemail} onChange={this.handleStudentEmail} />
+                        </div>
+                        <div>
+                            <button className='btn btn-success mx-3' id='validateButtonID' onClick={this.handleValidateStudent} disabled={validButtonState}>Validate</button>
+                        </div>
+                    </div>
                 </div>
                 <div id='information' className='container'>
                     {messageValidate && <ValidMessage valid={loadComponent} />}
                     {loadComponent && <StudentInfo actualStudent={actualStudent} />}
                 </div>
                 <div id='create' className='container pt-2'>
-                   <div className='mt-3'>
-                   <center>
-                    <button id='createButtonID' className='btn btn-primary' data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={this.handleCreateStudent} style={{ display: createButtonState ? 'none' : 'block' }} disabled={createButtonState}>Create</button>
-                   </center>
-                   </div>
+                    <div className='mt-3'>
+                        <center>
+                            <button id='createButtonID' className='btn btn-primary' data-bs-toggle="modal" data-bs-target="#createModal" onClick={this.handleCreateStudent} style={{ display: createButtonState ? 'none' : 'block' }} disabled={createButtonState}>Create</button>
+                        </center>
+                    </div>
                     {/* Modal */}
-                    <div class="modal fade modal-centered" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" hidden={this.state.disableModal}>
-                        <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h1 class="modal-title fs-5" id="exampleModalLabel">Add student</h1>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div className="modal fade modal-centered modal-lg" id="createModal" tabIndex="-1" aria-labelledby="modalLabel" aria-hidden="true" hidden={this.state.disableModal}>
+                        <div className="modal-dialog modal-dialog-centered">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h1 className="modal-title fs-5" id="modalLabel">Add student</h1>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
-                                <div class="modal-body">
-                                <center>
-                                <img src="./src/Assets/Images/check.png" className='w-50'/>
-                                <p className='text-success'>Student Created</p>
-                                </center>
+                                <div className="modal-body text-center">
+                                    <img src="./src/Assets/Images/check.png" className='w-50' />
+                                    <h2 className='text-success mt-3'>Student Created</h2>
                                 </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Continue</button>
+                                <div className="modal-footer text-center">
+                                    <button type="button" className="btn btn-primary" onClick={() => window.location.reload(false)}>Continue</button>
                                 </div>
                             </div>
                         </div>
--- Codigo Fuente/Aplicacion_Programa_Soporte_Interamericano/src/Principal/Add_Student/ValidMessage.jsx
+++ Codigo Fuente/Aplicacion_Programa_Soporte_Interamericano/src/Principal/Add_Student/ValidMessage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ValidMessage = (props) => {
   return (
     <div className='mt-4'>
-        {props.valid ? <h1 className='text-center text-success fw-bold'>Valid Email</h1> : <h1 className='text-center text-danger fw-bold'>Invalid Email</h1>}
+        {props.valid ? <h2 className='text-center text-success fw-bold'>Valid Email</h2> : <h2 className='text-center text-danger'>Invalid Email</h2>}
     </div>
   )
 }
