a41d0fb340a956940dd935d565ed2a9eecbece5a
DiegoBautista <dabautistac@correo.url.edu.gt>
2024-03-30 19:01:59 -0600

    Adición imágenes de prueba

---
+++ Codigo Fuente/Aplicacion_Programa_Soporte_Interamericano/src/Assets/Images/ImageIndex.js
@@ -0,0 +1,9 @@
+const ImageIndex =  {
+    student1: require('./dabautistac.jpg'),
+    student2: require('./vmbautistac.png'),
+    student3: require('./cfgonzalesc.png'),
+    student4: require('./jmmarroquinr.png'),
+    student5: require('./pcpisquiyp.png')
+}
+
+export default ImageIndex;
